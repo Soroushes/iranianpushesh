9640c824cb5c8608a4edc0b89232b59f2a1eb415
soroushes1 <esmailian.soroush246@gmail.com>
2023-01-27 00:45:33 +0330

    bug Fixed

--- src/helpers/priceDividerDecode.js
+++ src/helpers/priceDividerDecode.js
@@ -2,7 +2,7 @@ const priceDividerDecode = (price)=>{
     price = String(price) ;
     let result = '' ;
     for(let i = 0 ; i < price.length ; i++){
-        if (Number(price[i])) result += price[i];
+        if (Number(price[i]) || Number(price[i])===0) result += price[i];
     }
     return result ;
 }
